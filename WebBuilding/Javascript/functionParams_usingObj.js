5d414c0f329731833b7f7d149d4e97ef91387ad1
Christian B <christianbear1221@gmail.com>
2019-03-23 17:30:35 -0700

    ...

---
+++ WebBuilding/Javascript/functionParams_usingObj.js
@@ -0,0 +1,7 @@
+//function that uses obj as params
+// can be out of order!!!
+
+//runMe({facts:'it"s raining', something:"where it's dry", state:"   yeah well    ",})
+function runMe (arg){
+    console.log(arg.facts + arg.state + arg.something );
+}
