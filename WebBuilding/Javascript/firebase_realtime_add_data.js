f005972fb7215f2bbe839bc2d110c59a4e1f1e12
Christian B <christianbear1221@gmail.com>
2019-04-08 21:19:48 -0700

    working

---
+++ WebBuilding/Javascript/firebase_realtime_add_data.js
@@ -0,0 +1,8 @@
+//code that writes to firebase, realtime database
+function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
