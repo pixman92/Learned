3105f693dffdbc45309c42986e47f6077b45d7e0
Christian B <christianbear1221@gmail.com>
2019-05-08 18:20:56 -0700

    Update seconds_to_Date.js

--- WebBuilding/Javascript/seconds_to_Date.js
+++ WebBuilding/Javascript/seconds_to_Date.js
@@ -4,3 +4,10 @@ var newD = new Date();
 newD.setSeconds(seconds);
 
 db2.collection(path).where('date', '==', newD);
+
+
+//pulling seconds from web
+// id = date
+
+var toSecDate = new Date('<date>');
+toSecDate.getTime()/1000;
