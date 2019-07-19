dcb9a81e8c3d6cc1dc037993c677b9fd6c1d88c3
pixman92 <GOtrustD@verizon.net>
2019-07-19 16:17:55 -0700

    Create from_Date_To_Seconds_to_1_more_and_1_less.js

---
+++ WebBuilding/Javascript/from_Date_To_Seconds_to_1_more_and_1_less.js
@@ -0,0 +1,46 @@
+// function that takes date, 
+// adds 1 seconds
+// subtracts 1 second
+// then finds the date based on those date objects
+
+
+function findRoom(date){
+
+    // date1 = new Date("May 26,2019 7:48:05 PM");
+    // date2 = new Date("May 26,2019 7:48:07 PM");
+
+
+    // "July 19, 2019 3:23:19 PM"
+    
+    date = new Date(date);
+
+    var secondsLess = date.getTime()/1000;
+    secondsLess -= 1;
+    console.log(secondsLess);
+
+    var secondsMore = date.getTime()/1000;
+    secondsMore += 1;
+    console.log(secondsMore);
+
+
+    var newDateLess = new Date(null);
+    newDateLess.setTime(secondsLess*1000);
+    var newDateMore = new Date(null);
+    newDateMore.setTime(secondsMore*1000);
+
+    debugger
+
+
+
+    db.collection("/chatrooms").where("date", "<", newDateMore).where("date", ">", newDateLess).get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+}
