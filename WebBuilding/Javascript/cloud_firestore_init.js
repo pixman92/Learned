afdf4fd03b17ae668ea246065093fa8f83707c93
pixman92 <GOtrustD@verizon.net>
2019-07-21 21:32:21 -0700

    Update cloud_firestore_init.js

--- WebBuilding/Javascript/cloud_firestore_init.js
+++ WebBuilding/Javascript/cloud_firestore_init.js
@@ -2,5 +2,6 @@
 //add this line
 <script src="https://www.gstatic.com/firebasejs/5.9.3/firebase-firestore.js"></script>
 
+firebase.initializeApp(firebaseConfig);
 //to be able to init with this line
 var db = firebase.firestore();
