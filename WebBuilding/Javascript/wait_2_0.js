6435a9045501f76fe7015eee50db2b91dcc867bd
Christian B <christianbear1221@gmail.com>
2019-12-21 13:47:57 -0800

    wait 2.0

---
+++ WebBuilding/Javascript/wait_2_0.js
@@ -0,0 +1,40 @@
+//wait 2.0
+
+function runAll(){
+    one()
+    two()
+    three()
+}
+
+function one(){
+    stuff...
+    if(stuff==undefined){
+        one();
+    }
+}
+
+function two(){
+    stuff2...
+    if(stuff2==undefined){
+        two();
+    }
+}
+
+function three(){
+    stuff3...
+    if(stuff3==undefined){
+        three();
+    }
+}
+
+
+
+
+
+
+
+
+//========================================
+function wait(timout){
+    return new Promise(resolve=>setTimeout(resolve, timout));
+}
