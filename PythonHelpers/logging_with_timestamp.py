123cbb2f771a5b8c6e13201712a2274f9599d0d6
pixman92 <GOtrustD@verizon.net>
2020-02-11 16:23:04 -0800

    Create logging_with_timestamp.py

---
+++ PythonHelpers/logging_with_timestamp.py
@@ -0,0 +1,60 @@
+
+#python file that logs 1 string with a Date timestamp() variable 
+
+import sys
+from datetime import datetime
+
+
+
+fileName =""
+def runFirst():
+
+    global searchFuncName 
+    global fileName
+
+    try:
+        args = sys.argv
+    except:
+        print "args fail"
+    # print args
+    
+    if(args[1]=="file"):
+        fileName = args[2]
+        # print fileName
+        strLog()
+        makeTimestamp()
+        writeToFile(fileName, meInput)
+
+
+
+
+def writeToFile(writeTo, dataToWrite):
+
+    try:
+        f = open(writeTo,"a+")    
+        f.write("\n\n"+ todayTime + " --- " + dataToWrite)
+        f.close()
+    except:
+        print "failed to create file"
+
+
+meInput = ""
+
+def strLog():
+    print "What to Log? into, " + fileName
+    global meInput
+    meInput = ''
+    meInput = raw_input('\nEnter:')
+
+
+
+todayTime = ""
+def makeTimestamp():
+    format = "%Y, %m, %d - %H:%M:%S"
+    global todayTime
+    todayTime = datetime.now().strftime(format)
+    print todayTime 
+
+
+#========================================================
+runFirst()
