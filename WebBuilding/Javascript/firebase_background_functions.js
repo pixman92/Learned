259b91a21c0dc122112f364c202307e52b5366ac
pixman92 <GOtrustD@verizon.net>
2020-03-13 18:45:30 -0700

    Update firebase_background_functions.js

--- WebBuilding/Javascript/firebase_background_functions.js
+++ WebBuilding/Javascript/firebase_background_functions.js
@@ -38,7 +38,7 @@ async function whereMe(root, first, second){
 function secsToDate(seconds) {
     //function takes seconds from doc.data()
     //converts to a whole date String
-    var newD = new Date();
+    var newD = new Date(1970, 0, 1);
 
     newD.setSeconds(seconds);
 
