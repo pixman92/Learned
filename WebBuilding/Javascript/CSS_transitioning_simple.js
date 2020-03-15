6191914006cfd799f926916cab9a154a3039cee3
pixman92 <GOtrustD@verizon.net>
2020-03-15 02:37:15 -0700

    Create CSS_transitioning_simple.js

---
+++ WebBuilding/Javascript/CSS_transitioning_simple.js
@@ -0,0 +1,27 @@
+//transitions for CSS
+
+//start off with starting point
+.start{
+  height:
+  color:
+  shape:
+  position:
+   
+  transition: <property> <time>
+}
+
+.end{
+  height:
+  color:
+  shape:
+  position:
+   
+  transition: <property> <time>
+}
+
+//javascript
+
+    document.getElementById(id).classList.remove('start');
+    document.getElementById(id).classList.add('end');
+    
+    Vice Versa
