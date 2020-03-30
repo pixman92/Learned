f6be28990d24beeb231807394a76a86ece21961a
pixman92 <GOtrustD@verizon.net>
2020-03-29 17:20:30 -0700

    Create scripts_for_codepenio.js

---
+++ WebBuilding/Javascript/ReactJS/scripts_for_codepenio.js
@@ -0,0 +1,5 @@
+//made to make JSX work in this environment!
+
+<script src="https://codepen.io/chriscoyier/pen/yIgqi.js"></script>
+<script src="http://fb.me/react-0.13.0.js"></script>
+<script src="https://fb.me/JSXTransformer-0.12.0.js"></script>
