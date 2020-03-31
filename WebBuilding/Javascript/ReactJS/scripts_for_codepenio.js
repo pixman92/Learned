dbe62f58fee111044c71565eb3e2386193daab3a
pixman92 <GOtrustD@verizon.net>
2020-03-30 19:56:47 -0700

    Update scripts_for_codepenio.js

--- WebBuilding/Javascript/ReactJS/scripts_for_codepenio.js
+++ WebBuilding/Javascript/ReactJS/scripts_for_codepenio.js
@@ -1,5 +1,5 @@
 //made to make JSX work in this environment!
 
-<script src="https://codepen.io/chriscoyier/pen/yIgqi.js"></script>
-<script src="http://fb.me/react-0.13.0.js"></script>
-<script src="https://fb.me/JSXTransformer-0.12.0.js"></script>
+<script type="module" src="https://codepen.io/chriscoyier/pen/yIgqi.js"></script>
+<script type="module" src="http://fb.me/react-0.13.0.js"></script>
+<script type="module" src="https://fb.me/JSXTransformer-0.12.0.js"></script>
