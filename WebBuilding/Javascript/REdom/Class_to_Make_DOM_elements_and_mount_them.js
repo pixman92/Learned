d97061e7c521cd86e1f78bd87064d019726a6101
pixman92 <GOtrustD@verizon.net>
2020-05-30 12:54:51 -0700

    Create Class_to_Make_DOM_elements_and_mount_them.js

---
+++ WebBuilding/Javascript/REdom/Class_to_Make_DOM_elements_and_mount_them.js
@@ -0,0 +1,37 @@
+
+// pull imports
+const {el, mount} = redom;
+
+
+// class that takes in 'el', 'inner', and 'mountPoint'
+// then mounts said 'el' onto 'mountPoint'
+class MountMe{
+  constructor(){
+  }
+    
+  make(elMe, inner, mountPoint){
+    var tmp = el(elMe, inner);
+    console.log(mountPoint);
+    if(mountPoint==""){
+      mount(document.body, tmp);        // if left "" - then add to document.body
+    }else{
+   mount(document.getElementById(mountPoint), tmp);  
+    }
+    
+  }
+  
+  printMe(){
+    console.log(this.inner);
+    console.log(this.mountPoint);
+  }
+  
+}
+
+  
+var me = new MountMe();
+
+
+me.make('#me', 'hello', "");
+me.make('#nopeDiv', 'nope', "me")
+me.make('div', 'yup', "nopeDiv");
+me.printMe()
