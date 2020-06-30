d8a188f24989ccaccaf485814fef53efd3e652a9
pixman92 <GOtrustD@verizon.net>
2020-06-29 18:42:21 -0700

    Update Class_and_public_variables.js

--- WebBuilding/Javascript/Class_and_public_variables.js
+++ WebBuilding/Javascript/Class_and_public_variables.js
@@ -1,12 +1,27 @@
 class Message{  
-  makeAllPublic(fromEmail, toEmail, messageText){     //method for making passed in variables - into <this.> variables
+  constructor(fromEmail, toEmail, messageText){     //method for making passed in variables - into <this.> variables
         this.fromEmail = fromEmail;
         this.toEmail = toEmail;
         this.messageText = messageText;
         this.date = new Date;
-    }
+  }
+  
+  //returning functions - to be called later in different places - <this.returnFromEmail>
+  returnFromEmail(){
+      return this.fromEmail;
+  }
+  
+  returnToEmail(){
+    return this.toEmail;  
+}
+  returnMessageText(){
+    return this.messageText;
+  }
+  
+  
     print(){
-        console.log(this.messageText);
+        console.log(this.returnMessageText());
     }
+  
     
   }
