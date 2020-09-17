22d5b901a3051c7ba6c6b1e00fb6d0209c2f04a9
pixman92 <GOtrustD@verizon.net>
2020-09-16 22:42:12 -0700

    Update dbFirebase.js

--- WebBuilding/Javascript/dbFirebase.js
+++ WebBuilding/Javascript/dbFirebase.js
@@ -1,2 +1,11 @@
 var db1 = firebase.database();
 var db2 = firebase.firestore();
+
+
+
+
+    <script defer src="https://www.gstatic.com/firebasejs/7.1.0/firebase-app.js"></script>                                    
+    <script defer src="https://www.gstatic.com/firebasejs/7.1.0/firebase-database.js"></script>
+
+firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
