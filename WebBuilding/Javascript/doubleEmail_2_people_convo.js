a354e2bb522ba6eeab5a67fe29731d5b1415c37f
pixman92 <GOtrustD@verizon.net>
2020-10-16 19:20:27 -0700

    Create doubleEmail_2_people_convo.js

---
+++ WebBuilding/Javascript/doubleEmail_2_people_convo.js
@@ -0,0 +1,54 @@
+//=========================================
+var whereIds=[];
+var whereIds2=[];
+
+var tmp; var tmp2; var tmp3;
+async function doubleEmail(root, firstParam, thirdParam,  callback){
+    doubleWhereIds=[];
+    try{
+        let first = await one();
+        let second = await two(first);
+        let third = await three(second);
+    
+    }
+    catch(e){
+        throw(e);
+    }   
+
+    async function one(){
+        tmp = db.collection(root);
+    
+        tmp2 = tmp.where('myEmail', '==', firstParam);
+        
+        tmp3 = tmp.where('theirEmail', '==', thirdParam);
+    }
+
+    async function two(){
+        await tmp2.get().then(async (snap)=>snap.forEach(async (doc)=>{
+            console.log(doc)
+            await doubleWhereIds.push(doc.id);
+        }));
+
+        await tmp3.get().then(async(snap)=>snap.forEach(async (doc)=>{
+            console.log("2nd", doc)
+            await whereIds2.push(doc.id);
+        }));
+    }
+
+    async function three(){
+
+        for(var i=0; i<doubleWhereIds.length; i++){
+            for(var j=0; j<doubleWhereIds2.length; j++){
+                if(doubleWhereIds[i] == doubleWhereIds2[j]){
+                    console.log('matched', doubleWhereIds[i]);
+                }
+            }
+        }
+
+        console.log(doubleWhereIds);
+        
+        callback();
+
+    }
+
+}
