2d6ee31b092d4a04ead3b6feea2585b5a1008bb6
pixman92 <GOtrustD@verizon.net>
2021-09-07 17:11:24 -0700

    Create Divide_into_colums.js

---
+++ WebBuilding/CSS/Divide_into_colums.js
@@ -0,0 +1,9 @@
+  <!-- W3 School - CSS -->
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
+    
+    
+ w3schools.com/cssref/css3_pr_columns.asp
+
+div {
+  column-count: 3;
+}
