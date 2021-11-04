e7fef19299081f3903e19139d77e298fa6b8c3c7
pixman92 <GOtrustD@verizon.net>
2021-11-03 21:21:22 -0700

    Create getKeys.js

---
+++ WebBuilding/Javascript/getKeys.js
@@ -0,0 +1,6 @@
+function getKeys(){
+  //this is a really cool function
+  //that pulls out the keys from Obj{} key/value pairs
+  var myKeys = Object.keys(myJSON.JSONobj.innerArray[0]);
+  console.log(myKeys);
+}
