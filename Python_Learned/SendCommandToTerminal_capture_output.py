c5fa0463170a6494115d27c8b34c597ad973053d
pixman92 <GOtrustD@verizon.net>
2022-01-15 15:51:31 -0800

    Update SendCommandToTerminal_capture_output.py

--- Python_Learned/SendCommandToTerminal_capture_output.py
+++ Python_Learned/SendCommandToTerminal_capture_output.py
@@ -1,6 +1,6 @@
 # sends a command to terminal
 # captures the output
 # output is stored in a split lines array
-    processLog = subprocess.check_output(['git log --oneline'], shell=True)
+    processLog = subprocess.check_output('git log --oneline', shell=True)
     print (processLog)
     processLogOut = processLog.splitlines()
