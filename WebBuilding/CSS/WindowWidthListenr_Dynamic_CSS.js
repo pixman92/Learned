ee30c3cbd6b63896aa923669db1235f25249acd1
pixman92 <GOtrustD@verizon.net>
2022-01-19 22:59:52 -0800

    Create WindowWidthListenr_Dynamic_CSS.js

---
+++ WebBuilding/CSS/WindowWidthListenr_Dynamic_CSS.js
@@ -0,0 +1,10 @@
+window.addEventListener('resize', function(event){
+    var newWidth = window.innerWidth;
+    var newHeight = window.innerHeight; 
+});
+
+//use this in Pair with: 
+changeCSS();
+
+// found at THIS repository:
+https://github.com/pixman92/CSS_Strip
