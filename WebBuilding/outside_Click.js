1ddd7074a1cac5ff3d746ae729e765b29fe49a67
pixman92 <GOtrustD@verizon.net>
2022-01-24 21:50:11 -0800

    Update outside_Click.js

--- WebBuilding/outside_Click.js
+++ WebBuilding/outside_Click.js
@@ -4,3 +4,30 @@ $(document).click((event) => {
     // the click occured outside '#element'
   }        
 });
+
+
+// =================Example===========
+
+// is for the div -> when pressed -> turned into a TEXTAREA
+
+var savedDeckNameFrontScreen = $('.deckNameText').text();           //saving text before edit
+    $('.deckNameText').on('click', (event)=>{                           //setting up DOM eventlistener
+        event.stopPropagation();
+        $('.deckNameText').html('<textarea class="deckNameTextArea">'+savedDeckNameFrontScreen+'</textarea>')       //transfer to NEW HTML
+
+      
+        console.log('holding1', holding1);
+        $('.deckNameText').off();
+});       
+
+// when clicked outside, save the change!
+$('body').on('click', (event) => {                                              //event for, clicking outside
+        if (!$(event.target).closest('.deckNameTextArea').length) {                     // if click is outside
+            // the click occured outside '#element'
+            console.log('i ran out of the box');     
+            holding1 = $('.deckNameTextArea').val(); 
+            $('.deckNameText').html();                                          //reset html
+            $('.deckNameText').html('<div>'+ holding1 + '</div>');
+            makeThemEditable();
+        }        
+});
