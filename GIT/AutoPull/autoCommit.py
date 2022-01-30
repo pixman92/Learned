b94862cb3b74c820324b2a5c2ebcd4366f4cfbab
pixman92 <GOtrustD@verizon.net>
2022-01-29 22:18:56 -0800

    Update autoCommit.py

--- GIT/AutoPull/autoCommit.py
+++ GIT/AutoPull/autoCommit.py
@@ -5,13 +5,20 @@ import time
 import subprocess
 import os
 import signal
+import datetime
 
 
 def run():
-    subprocess.call(["git", "commit", "-m", "10 minute Looping Commit"])
 
+    todayDate = str(datetime.date.today())
+    subprocess.call(["git", "add", "*"])
+    time.sleep(1)
+    subprocess.call(["git", "commit", "-m", todayDate +" - 10 minute Looping Commit"])
+    subprocess.call(["git", "push"])
+    time.sleep(1)
+run()
 
 for x in range(0, 1000):
-    time.sleep(3)
+    time.sleep(60*10)
 
     run()
