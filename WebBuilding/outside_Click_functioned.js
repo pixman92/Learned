9d485dcc8e0b67e66479c967e61d541a9180757e
pixman92 <GOtrustD@verizon.net>
2022-03-30 19:37:22 -0700

    Update outside_Click_functioned.js

--- WebBuilding/outside_Click_functioned.js
+++ WebBuilding/outside_Click_functioned.js
@@ -1,37 +1,18 @@
-function outOfCode(clickedItem, closestElement){
-  
-  // code that allows function to run - if clicked out of bounds
-//   $(document).click((event) => {
-//     if (!$(event.target).closest('#'+closestElement).length) {
-//       // the click occured outside '#element'
-//     }        
-//   });
-
-
-  // =================Example===========
-
-  // is for the div -> when pressed -> turned into a TEXTAREA
-
-  $('.'+clickedItem).on('click', (event)=>{                           //setting up DOM eventlistener
-      var savedNameFrontScreen = $('.'+clickedItem).text();           //saving text before edit
-      event.stopPropagation();
-      $('.'+clickedItem).html('<textarea class="newName">'+savedNameFrontScreen+'</textarea>')       //transfer to NEW HTML
-
-
-      console.log('SavedText: ', savedNameFrontScreen);
-      $('.'+clickedItem).off();
-  });       
-
-  // when clicked outside, save the change!
-  $('body').on('click', (event) => {                                              //event for, clicking outside
-          if (!$(event.target).closest('.'+closestElement).length) {                     // if click is outside
-              // the click occured outside '#element'
-              console.log('i ran out of the box');     
-              savedNameFrontScreen = $('.newName').val(); 
-              $('.'+clickedItem).html();                                          //reset html
-              $('.'+clickedItem).html('<div>'+ savedNameFrontScreen + '</div>');
-          }        
-       $('body').off();
-//        makeThemEditable();
+function eventListeners(){
+  document.getElementsByClassName('changeMe')[0].addEventListener('click', ()=>{
+    change();
   });
 }
+
+eventListeners();
+
+function change(){
+  document.getElementsByClassName('changeMe')[0].setAttribute("contenteditable", "true");
+
+}
+
+function print(){
+   var text = document.getElementsByClassName('changeMe')[0].innerText;
+  console.log(text);
+}
+
