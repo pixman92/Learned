ce658cd12654148ce7271930380cce17eb6cec06
pixman92 <GOtrustD@verizon.net>
2022-03-30 18:54:08 -0700

    Create outside_Click_functioned.js

---
+++ WebBuilding/outside_Click_functioned.js
@@ -0,0 +1,36 @@
+function outOfCode(clickedItem, closestElement){
+  
+  // code that allows function to run - if clicked out of bounds
+//   $(document).click((event) => {
+//     if (!$(event.target).closest('#'+closestElement).length) {
+//       // the click occured outside '#element'
+//     }        
+//   });
+
+
+  // =================Example===========
+
+  // is for the div -> when pressed -> turned into a TEXTAREA
+
+      $('.'+clickedItem).on('click', (event)=>{                           //setting up DOM eventlistener
+          var savedNameFrontScreen = $('.'+clickedItem).text();           //saving text before edit
+          event.stopPropagation();
+          $('.'+clickedItem).html('<textarea class="newName">'+savedNameFrontScreen+'</textarea>')       //transfer to NEW HTML
+
+
+          console.log('holding1', holding1);
+          $('.'+clickedItem).off();
+  });       
+
+  // when clicked outside, save the change!
+  $('body').on('click', (event) => {                                              //event for, clicking outside
+          if (!$(event.target).closest('.'+savedNameFrontScreen).length) {                     // if click is outside
+              // the click occured outside '#element'
+              console.log('i ran out of the box');     
+              holding1 = $('.'+savedNameFrontScreen).val(); 
+              $('.'+clickedItem).html();                                          //reset html
+              $('.'+clickedItem).html('<div>'+ holding1 + '</div>');
+          }        
+       $('body').off();
+//        makeThemEditable();
+  });
