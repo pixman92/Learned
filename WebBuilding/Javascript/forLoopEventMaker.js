f841f28ee3cb32a3078378b892ef535bc097ecae
pixman92 <GOtrustD@verizon.net>
2022-04-22 20:20:16 -0700

    Update forLoopEventMaker.js

--- WebBuilding/Javascript/forLoopEventMaker.js
+++ WebBuilding/Javascript/forLoopEventMaker.js
@@ -1,6 +1,6 @@
   // what's special about this code? it uses 'let' over 'var'
   // allowing for new indexes to be made at each turn over of the for loop
-  for(let i=0; i<theBIGJSON.JSONobj.innerArray[1].length-1; i++){
+  for(let i=0; i<theBIGJSON.JSONobj.innerArray[1].length; i++){
       
       $('.class'+i).on('blur keyup paste input', ()=>{
           console.log('i', i);
