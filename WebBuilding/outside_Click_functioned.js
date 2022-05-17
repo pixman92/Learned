d7bf40672b40ba5f2616fe1d55e3e9275d36656e
pixman92 <GOtrustD@verizon.net>
2022-05-17 10:19:50 -0700

    Update outside_Click_functioned.js

--- WebBuilding/outside_Click_functioned.js
+++ WebBuilding/outside_Click_functioned.js
@@ -1,18 +1,37 @@
-function eventListeners(){
-  document.getElementsByClassName('changeMe')[0].addEventListener('click', ()=>{
-    change();
-  });
-}
+<div class="outer">
+  <div class="changeMe">Hello</div>
+</div>
 
-eventListeners();
-
+// =================
 function change(){
   document.getElementsByClassName('changeMe')[0].setAttribute("contenteditable", "true");
 
 }
 
-function print(){
+function printMe(){
    var text = document.getElementsByClassName('changeMe')[0].innerText;
   console.log(text);
 }
 
+function eventListeners(){
+  document.getElementsByClassName('changeMe')[0].addEventListener('click', ()=>{
+    change();
+  });
+  
+  
+  window.addEventListener('click', function(e){   
+    if (document.getElementsByClassName('changeMe')[0].contains(e.target)){
+      // Clicked in box
+      console.log('in');
+    } else{
+      // Clicked outside the box
+      console.log('out');
+      printMe();
+    }
+  });
+
+}
+
+eventListeners();
+
+// =================
