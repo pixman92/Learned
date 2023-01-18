46f5849734e6960f7a01a4302fa996bd9edbbbfd
pixman92 <GOtrustD@verizon.net>
2023-01-18 10:59:42 -0800

    Create init v1.js

---
+++ WebBuilding/ReactJS/init v1.js
@@ -0,0 +1,21 @@
+import Reactm, { useState } from "https://esm.sh/react@18.2.0";
+import ReactDOM from "https://esm.sh/react-dom@18.2.0";
+import * as reactRouter from "https://cdn.skypack.dev/react-router@6.6.2";
+
+
+// =================
+function Main() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+
+
+
+
+const rootNode = document.getElementById("root");
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(Main));
