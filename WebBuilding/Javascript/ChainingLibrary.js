50d09ce2bb01f3049aa2e6d290bd6a434637ad25
pixman92 <GOtrustD@verizon.net>
2023-10-25 21:45:33 -0700

    Create ChainingLibrary.js

---
+++ WebBuilding/Javascript/ChainingLibrary.js
@@ -0,0 +1,42 @@
+class Chainer {
+  constructor() {
+    this._queue = [];
+  }
+
+  add(fn) {
+    this._queue.push(fn);
+    return this;
+  }
+
+  async next() {
+    const fn = this._queue.shift();
+    if (fn) {
+      await fn();
+      await this.next();
+    }
+  }
+
+  async start() {
+    await this.next();
+  }
+}
+
+const chain = (fns) => {
+  const chainer = new Chainer();
+  for (const fn of fns) {
+    chainer.add(fn);
+  }
+  return chainer;
+};
+
+const example = async () => {
+  console.log("Starting");
+  await chain([
+    () => console.log("First function"),
+    () => console.log("Second function"),
+    () => console.log("Third function"),
+  ]).start();
+  console.log("Finished");
+};
+
+example();
